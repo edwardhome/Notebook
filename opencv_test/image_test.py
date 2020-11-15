faeb0e7a406bb384f150fa7209b46680133c480c
Defu <edward61221@gmail.com>
2020-11-15 17:41:43 +0800

    image show

--- opencv_test/image_test.py
+++ opencv_test/image_test.py
@@ -12,3 +12,11 @@ C_size = np.shape(C)
 print(A_size)
 print(B_size)
 print(C_size)
+
+cv2.imshow('B',(A[:,:,0])) #取得影像藍色數據
+cv2.imshow('G',(A[:,:,1])) #取得影像綠色數據
+cv2.imshow('R',(A[:,:,2])) #取得影像紅色數據
+cv2.imshow('all',(A[:,:,:])) #取得影像所有數據
+cv2.imshow('gray',B) #取得影像數據
+
+cv2.waitKey(0)
